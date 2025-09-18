30f0d976dc5d029b36617c9380f90bc92881fc5b
a9irudhh <01fe22bcs264@kletech.ac.in>
2025-09-19 00:13:26 +0530

    added basic directory structure and few utility files

---
+++ backend/src/db/index.db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        if(!process.env.MONGODB_URI){
+            throw new Error("Mongo URI is missing")
+        }
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        console.log(`Connected to DB: ${connectionInstance.connection.host}`)
+        
+    } catch (error) {
+        console.error("Error while connecting to DB", error)
+    }
+} 
+
+export default connectDB
